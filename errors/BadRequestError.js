3d3c5a32ca259420f262ff3d495bc0fe239230c6
Timophey Shalonsky <shalonsky.tim@yandex.ru>
2022-12-04 22:42:04 +0300

    clear main branch

--- errors/BadRequestError.js
+++
@@ -1,9 +0,0 @@
-module.exports = class BadRequestError extends Error {
-  constructor(message) {
-    super(message);
-    this.errorCode = 400;
-    this.statusCode = 400;
-    this.errorMessage = message;
-    this.name = 'BadRequestError';
-  }
-};
